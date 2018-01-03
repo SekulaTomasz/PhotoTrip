d1822a787233979eda9d87ea343343ee5ad32a29
SekulaTomasz <sekula.tomasz1993@gmail.com>
2018-01-03 16:20:09 +0100

    .

---
+++ PhotoTrip.Infrastructure/Database/PhotoTripContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoTrip.Infrastructure.Database
+{
+    public class PhotoTripContext : DbContext
+    {
+        public PhotoTripContext(DbContextOptions<PhotoTripContext> conn) : base(conn)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+    }
+}
